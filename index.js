75f10412073890fb7b4b413d7a256eb2960e1919
scuba75 <scuba75@gmail.com>
2026-05-08 07:08:32 -0500

    update

---
+++ index.js
@@ -0,0 +1,137 @@
+'use strict'
+const { DataApiClient } = require('rqlite-js')
+const log = require('./logger')
+
+const CACHE_HOSTS = process.env.REACTION_CACHE_URL || ['http://data-cache-0.data-cache-internal.datastore.svc.cluster.local:4001', 'http://data-cache-1.data-cache-internal.datastore.svc.cluster.local:4001', 'http://data-cache-2.data-cache-internal.datastore.svc.cluster.local:4001']
+
+const dataApiClient = new DataApiClient(CACHE_HOSTS)
+let TABLE_SET = new Set()
+
+async function checkTableExists(table){
+  try{
+    if(TABLE_SET.has(table)) return true
+
+    let sql = `SELECT name FROM sqlite_master WHERE type='table' AND name='${table}'`
+    let dataResults = await dataApiClient.query(sql)
+    if(dataResults?.hasError()){
+      log.error(dataResults?.getFirstError())
+      return
+    }
+    if(dataResults?.get(0)?.data?.name == table){
+      TABLE_SET.add(table)
+      return true
+    }
+  }catch(e){
+    log.error(e)
+  }
+}
+async function createTable(table){
+  try{
+    let sql = `CREATE TABLE IF NOT EXISTS "${table}" (id TEXT PRIMARY KEY, data TEXT NOT NULL, ttl INTEGER)`
+    let dataResults = await dataApiClient.execute(sql)
+    if(dataResults?.hasError()){
+      log.error(dataResults?.getFirstError())
+      return
+    }
+    TABLE_SET.add(table)
+    return true
+  }catch(e){
+    log.error(e)
+  }
+}
+async function tableCheck(table){
+  try{
+    if(!table) return;
+
+    let status = await checkTableExists(table)
+    if(!status) status = await createTable(table)
+
+    return status
+  }catch(e){
+    log.error(e)
+  }
+}
+async function all(table){
+  try{
+    if(!table) return
+    let status = await checkTableExists(table)
+    if(!status) return
+
+    let sql = `SELECT id, data FROM "${table}"`
+    let dataResults = await dataApiClient.query(sql)
+    if(dataResults.hasError()){
+      log.error(dataResults?.getFirstError())
+      return
+    }
+    let result = dataResults?.toArray()?.filter(x=>x.?.data)
+    if(result?.length > 0){
+      let res = {}
+      for(let i in result){
+        if(!result[i]?.id || !result[i]?.data) continue
+        res[result[i].id] = JSON.parse(result[i].data)
+      }
+      return res
+    }
+  }catch(e){
+    log.error(e)
+  }
+}
+async function del(table, key){
+  try{
+    if(!table) return
+    let status = await checkTableExists(table)
+    if(!status) return
+
+    let sql = `DELETE FROM "${table}" WHERE id=${key.toString()}`
+    let dataResults = await dataApiClient.execute(sql)
+    if(dataResults?.hasError()){
+      log.error(dataResults?.getFirstError())
+      return
+    }
+    return dataResults?.get(0)?.getRowsAffected()
+  }catch(e){
+    log.error(e)
+  }
+}
+
+async function get(table, key){
+  try{
+    if(!table) return
+    let status = await checkTableExists(table)
+    if(!status) return
+
+    let sql = `SELECT data FROM "${table}" WHERE id=${key?.toString()}`
+    let dataResults = await dataApiClient.query(sql)
+    if(dataResults.hasError()){
+      log.error(dataResults?.getFirstError())
+      return
+    }
+    let result = dataResults?.get(0)
+    if(result?.data?.data) return JSON.parse(result?.data?.data)
+  }catch(e){
+    log.error(e)
+  }
+}
+async function set(table, key, value){
+  try{
+    if(!table || !key || !value) return
+    let status = await tableCheck(table)
+    if(!status){
+      log.error(`Could not create gamedata table for ${table}`)
+      return
+    }
+    let sql = [
+      [`INSERT INTO "${table}" (id, data, ttl) VALUES(:id, :data, ${Date.now()}) ON CONFLICT(id) DO UPDATE set data=:data, ttl=${Date.now()}`, { id: key.toString(), data: JSON.stringify(value) }]
+    ]
+    let dataResults = await dataApiClient.execute(sql)
+    if(dataResults?.hasError()){
+      log.error(dataResults?.getFirstError())
+      return
+    }
+    return dataResults?.get(0)?.getLastInsertId()
+  }catch(e){
+    log.error(e)
+  }
+}
+
+module.exports = { all, del, get, set }
---
+++ logger.js
@@ -0,0 +1,20 @@
+function getTimeStamp(timestamp){
+  if(!timestamp) timestamp = Date.now()
+  let dateTime = new Date(timestamp)
+  return dateTime.toLocaleString('en-US', { timeZone: 'Etc/GMT+5', hour12: false })
+}
+export function error(err, table_name){
+  try{
+    console.error(`${getTimeStamp(Date.now())} ERROR [data-cache] ${err}`)
+    if(err?.stack) console.error(err)
+  }catch(e){
+    console.error(e)
+  }
+}
+export function info(msg, table_name){
+  try{
+    console.log(`${getTimeStamp(Date.now())} INFO [data-cache] ${msg}`)
+  }catch(e){
+    console.error(e)
+  }
+}
